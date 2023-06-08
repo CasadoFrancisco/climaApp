89b1a584e1f4b67cef811286f83700fadce28252
francisco casado <122122722+CasadoFrancisco@users.noreply.github.com>
2023-06-08 17:31:01 -0300

    change

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomeComponent } from "./page/home/Home";
+
+
+export const RoutesComponent:React.FC = () =>{
+    return(
+        <Routes>
+            <Route path="/" element={<HomeComponent/>}/>
+        </Routes>
+    )
+}
---
+++ frontend/src/page/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css"
+import { NavbarComponent } from "../../components/navbar/navbar";
+
+
+export const HomeComponent: React.FC =() =>{
+    return(
+        <>
+        <NavbarComponent/>
+        
+        </>
+    )
+}
+
